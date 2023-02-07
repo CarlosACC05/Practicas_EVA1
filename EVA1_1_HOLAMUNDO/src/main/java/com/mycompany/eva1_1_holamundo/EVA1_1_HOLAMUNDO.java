07bcc02053aa914bb1051aadcb2edd7f02ccb01a
CarlosACC05 <124701900+CarlosACC05@users.noreply.github.com>
2023-02-07 09:39:41 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/com/mycompany/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,18 @@
+package com.mycompany.eva1_1_holamundo;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x;
+        
+        x=1;
+        
+        System.out.println(x+1);
+    }
+   
+}
