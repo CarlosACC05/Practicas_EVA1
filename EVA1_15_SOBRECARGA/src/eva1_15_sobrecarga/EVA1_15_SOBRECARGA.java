dcfb9aa68ce2b3b6727a5dbb9de689c59910ceb0
CarlosACC05 <124701900+CarlosACC05@users.noreply.github.com>
2023-02-26 14:18:19 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        /*DIFERENTES PRINTLN: SON 10 DIFERENTES 
+        METODOS, OARA IMPRIMIR DIFERENTES TIPOS DE DATOS*/
+        /*PARA CREAR SOBRECARGA DE METODOS 
+        LA FIRMA DEL METODOS DEBE DE SER UNICA*/
+        System.out.println("HOLA MUNDO");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+
+    }
+
+}
